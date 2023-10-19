ba6c7838e704b7599034febbdc9ccb6087d02a39
Alekseev Andrey <alekseev.andrew00@gmail.com>
2023-10-19 22:12:42 +0300

    Add readConfig method via viper

--- configuration/config.go
+++ configuration/config.go
@@ -1,5 +1,10 @@
 package configuration
 
+import (
+	"github.com/spf13/viper"
+	"strings"
+)
+
 type Config struct {
 	System struct {
 		Http struct {
@@ -23,4 +28,17 @@ type Config struct {
 	}
 	ScanDelay      int
 	JobGrepPattern string
+}
+
+func ReadConfig(path string) (config *Config, err error) {
+	viper.SetConfigFile(path)
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return nil, err
+	}
+	err = viper.Unmarshal(&config)
+	return config, err
 }
