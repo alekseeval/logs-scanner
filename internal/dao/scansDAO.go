b89cad382aceac48c2e44d4ba25611da7267b33d
Alekseev Andrey <alekseev.andrew00@gmail.com>
2023-11-21 22:32:12 +0300

    Added rwlock for in-service-storage

--- internal/dao/scansDAO.go
+++ internal/dao/scansDAO.go
@@ -3,18 +3,23 @@ package dao
 import (
 	"github.com/sirupsen/logrus"
 	"scan_project/internal/model"
+	"sync"
 )
 
 type ScansDao struct {
 	jobsScans     map[daoKey][]model.JobScan
+	jMutex        sync.RWMutex
 	servicesScans map[daoKey][]model.ServiceScan
+	sMutex        sync.RWMutex
 	logger        *logrus.Entry
 }
 
 func NewScansDao(logger *logrus.Entry) ScansDao {
 	return ScansDao{
 		jobsScans:     make(map[daoKey][]model.JobScan),
+		jMutex:        sync.RWMutex{},
 		servicesScans: make(map[daoKey][]model.ServiceScan),
+		sMutex:        sync.RWMutex{},
 		logger:        logger,
 	}
 }
@@ -28,10 +33,12 @@ func (sd *ScansDao) GetJobsScans(clusterName string, namespace string) []model.J
 	sd.logger.
 		WithField("params", []string{clusterName, namespace}).
 		Debug("Get jobs scans")
+	sd.jMutex.RLock()
 	scans := sd.jobsScans[daoKey{
 		clusterName: clusterName,
 		namespace:   namespace,
 	}]
+	sd.jMutex.RUnlock()
 	if scans != nil {
 		return scans
 	} else {
@@ -48,7 +55,9 @@ func (sd *ScansDao) UpdateJobsScans(clusterName string, namespace string, jobsSc
 		clusterName: clusterName,
 		namespace:   namespace,
 	}
+	sd.jMutex.Lock()
 	sd.jobsScans[key] = jobsScans
+	sd.jMutex.Unlock()
 	return nil
 }
 
@@ -56,10 +65,12 @@ func (sd *ScansDao) GetServicesScans(clusterName string, namespace string) []mod
 	sd.logger.
 		WithField("params", []string{clusterName, namespace}).
 		Debug("Get services scans")
+	sd.sMutex.RLock()
 	scans := sd.servicesScans[daoKey{
 		clusterName: clusterName,
 		namespace:   namespace,
 	}]
+	sd.sMutex.RUnlock()
 	if scans != nil {
 		return scans
 	} else {
@@ -76,6 +87,8 @@ func (sd *ScansDao) UpdateServicesScans(clusterName string, namespace string, se
 		clusterName: clusterName,
 		namespace:   namespace,
 	}
+	sd.sMutex.Lock()
 	sd.servicesScans[key] = servicesScans
+	sd.sMutex.Unlock()
 	return nil
 }
