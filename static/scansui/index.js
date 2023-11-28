ff304185c5bcffe282553e1ede23e7478aec761b
Alekseev Andrey <alekseev.andrew00@gmail.com>
2023-11-28 22:17:29 +0300

    Save changes in static

---
+++ static/scansui/index.js
@@ -0,0 +1,8 @@
+class App extends React.Component {
+    render() {
+        return ( <
+                div > This is Naveen!! < /div>
+        );
+    }
+}
+ReactDOM.render( <App / >, document.getElementById("root"));
